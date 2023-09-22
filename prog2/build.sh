311fb5208711b86fcca4ee8b9f0cbc871af765a9
asphy396 <mertalbayrak@gmail.com>
2023-09-22 17:02:33 -0400

    finish prog2

--- prog2/build.sh
+++ prog2/build.sh
@@ -2,4 +2,5 @@
 
 set -xe
 
-gcc -g -o partial program2_malbayra_nth_partial_sum.c
+gcc -Wall -Wextra -o partial program2a_malbayra_nth_partial_sum.c
+gcc -Wall -Wextra -o data program2b_malbayra_data_breaches.c -lm
